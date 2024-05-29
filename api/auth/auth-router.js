5804ecaac6819de22d538afb0cf3776f7a0d60ce
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2024-05-29 11:50:08 -0400

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router()
+const Middle = require('./auth-middleware')
+const User = require('../users/users-model')
 
 
-router.post('/register', (req, res) => {
-	res.json({message: 'register working'})
+router.post('/register', Middle.protect, async (req, res) => {
+	const { username, password } = req.body
+	const hash = bcrypt.hashSync(password, 10)
+	const newUser = {
+		username, 
+		password: hash
+	}
+	const result = await User.add(newUser)
+	res.status(201).json({ message: `nice to have you, ${result.username}` });
 })
 
 router.post('/login', (req, res) => {
